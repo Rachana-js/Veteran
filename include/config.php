4cd398a613f3a98794e57c56165bafa9c5b4c6a3
Rachana J S <103162318+Rachana-js@users.noreply.github.com>
2024-10-28 15:49:29 +0530

    Create config.php

---
+++ include/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'veteran');
+
+// admin user and password
+define('ADMIN_LOGIN_ID', 'admin@gmail.com');
+define('ADMIN_PASSWORD', 'admin');
+
+define('PROJECT_NAME', 'Online Pet Shop System');
+
