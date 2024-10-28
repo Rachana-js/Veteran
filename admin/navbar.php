d1281691a3c339a7580a397ba97fc395a4c3eff7
Rachana J S <103162318+Rachana-js@users.noreply.github.com>
2024-10-28 13:32:40 +0530

    Create navbar.php

---
+++ admin/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#"><?php echo PROJECT_NAME; ?></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="animals.php">Animals</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="food.php">Food</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="disease.php">Disease</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="doctors.php">Doctors</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="users.php">Users</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
