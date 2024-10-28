aefa60e16f65964bacd19ecbbb99907f4925dc38
Rachana J S <103162318+Rachana-js@users.noreply.github.com>
2024-10-28 15:51:15 +0530

    Create head.php

---
+++ include/head.php
@@ -0,0 +1,8 @@
+<?php require_once '../include/config.php' ?>
+<head>
+    <title><?php echo PROJECT_NAME; ?></title>
+    <link href="../static/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../static/css/dashboard.css" rel="stylesheet">
+    <link href="../static/js/datepicker/jquery-ui.min.css" rel="stylesheet">
+    <link href="../static/css/custom.css" rel="stylesheet">
+</head>
